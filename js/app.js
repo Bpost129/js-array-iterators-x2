6e69d3618a1fbf2dafe179f1982bfb037dfc0074
Bpost129 <Bpost129@yahoo.com>
2025-11-21 09:44:46 -0500

    complete problem 1

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@ const inventors = [
 // Array.prototype.filter()
 // 1. Filter the array of inventors into a new array containing only the inventors born in the 1500's.
 
-
+const oldInventors = inventors.filter(inv => inv.year >= 1500 && inv.year < 1600)
+console.log(oldInventors)
 
 // Array.prototype.map()
 // 2. Map the array of the inventors into a new array containing objects with just the first and last names as properties.
